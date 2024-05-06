00bd8a8117eda2a538800e0e4c09ffce291aebbf
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-06 10:26:17 -0700

    Task 0: 🛠️ ✨ Implement wait_random coroutine to introduce variability with random delays up to max_delay seconds - PEP8 correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,14 +6,16 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds
-    (exclusive on the upper bound) and eventually returns it.
+    Asynchronous coroutine that waits for a random delay
+    between 0 and max_delay seconds (exclusive on the upper
+    bound) and eventually returns it.
 
     Args:
         max_delay (int): Maximum delay in seconds (default is 10).
 
     Returns:
-        float: Random delay between 0 and max_delay seconds (exclusive on the upper bound).
+        float: Random delay between 0 and max_delay seconds
+        (exclusive on the upper bound).
     """
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
