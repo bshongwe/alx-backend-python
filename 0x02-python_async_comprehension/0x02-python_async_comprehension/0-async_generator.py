da4ec1a4fe984b8cd9ff0a9d0984809771c36c61
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-06 18:32:45 -0700

    Task 0: ✨ Add async_generator for asynchronous random number generation 🎲

--- 0x02-python_async_comprehension/0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,19 @@
 """
 Module: Task 0 - Async Generator
 """
+import asyncio
+import random
+from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    Generates a sequence of 10 numbers.
+    Asynchronously generates a sequence of 10 random
+    floating-point numbers.
+
+    Yields:
+        float: A random floating-point number between 0 and 10.
     """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
