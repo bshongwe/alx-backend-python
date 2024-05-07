3de32d62e29600e2eceb3754534b1074c440d56a
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-06 18:14:13 -0700

    Task 0: 🛠️ task init - with prototype

---
+++ 0x02-python_async_comprehension/0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module: Task 0 - Async Generator
+"""
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Generates a sequence of 10 numbers.
+    """
